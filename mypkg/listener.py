e68920ab72c3ae4fa312a783cd5e0a3697372132
ryoma minami <minami@DESKTOP-GF64790.localdomain>
2022-11-24 13:11:46 +0900

    Add

---
+++ mypkg/listener.py
@@ -0,0 +1,12 @@
+ import rclpy
+ from rclpy.node import Node
+ from person_msgs.msg import Person
+ 
+ def cb(msg):
+     global node
+     node.get_logger().info("Listen: %s" % msg)
+     
+ rclpy.init()
+ node = Node("listener")
+ pub = node.create_subscription(person, "person", cb, 10)
+ rclpy.spin(node)
